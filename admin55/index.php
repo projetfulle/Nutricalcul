bae5939f96837cb7aa808cae2f05e6ddb98d5c9f
projetfulle <projetfulle@gmail.com>
2019-03-28 22:34:44 +0000

    Add files via upload

---
+++ admin55/index.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'dbconfig.php';
+?>
+<?php include_once 'header.php'; ?>
+
+<div class="clearfix"></div>
+
+<div class="container">
+      <main role="main" class="inner cover">
+              <h1 class="cover-heading">Vous devez contacter l'administrateur.</h1>
+              <p class="lead">Bonjour à vous, cette adresse n'est pas reservée à tout le monde. N'insistez pas. Si vous êtes l'un des propriétaire, contactez votre administrateur.</p>
+              <p class="lead">
+                <a href="../" class="btn btn-lg btn-secondary">Retour sur la recherche</a>
+              </p>
+            </main>
+   
+       
+</div>
+
+<?php include_once 'footer.php'; ?>
